7ae3fa922ca084c18e5052a55739a17477ae83bc
J Cheng <JC@JC.local>
2017-01-29 15:03:09 -0800

    Initial Commit

---
+++ p2_script.js
@@ -0,0 +1,28 @@
+function Refresh() {
+    window.parent.location = window.parent.location.href;
+}
+
+$(document).ready(function() {
+  // Your Javascript code here
+  // SAMPLE: Grab earthquake data from USGS feed
+  var EARTHQUAKE_API = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+  $.get(EARTHQUAKE_API)
+    .done(function(res) {
+      // Output earthquakes to the page
+      console.log(res);
+      simpleEarthquakeDisplay(res.features);
+    })
+    .fail(function(error) {
+      // Do something with the error
+    })
+
+  // SAMPLE: Display the earthquake titles on the page
+  function simpleEarthquakeDisplay(quakes) {
+    var container = $('#sample').empty();
+    quakes.forEach(function(quake) {
+      var quakeEl = $('<li></li>')
+        .text(quake.properties.title)
+        .appendTo(container);
+    });
+  }
+});
