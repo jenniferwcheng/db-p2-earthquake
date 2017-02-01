9962a6dd243247b821f973d1743678a71f534ea6
J Cheng <JC@JC.local>
2017-02-01 11:01:32 -0800

    Added map, fixed font, added a pop-up tab

--- p2_script.js
+++ p2_script.js
@@ -1,28 +1,50 @@
-function Refresh() {
-    window.parent.location = window.parent.location.href;
-}
-
 $(document).ready(function() {
-  // Your Javascript code here
-  // SAMPLE: Grab earthquake data from USGS feed
+
+  // Grab earthquake data from USGS feed
   var EARTHQUAKE_API = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
   $.get(EARTHQUAKE_API)
     .done(function(res) {
       // Output earthquakes to the page
       console.log(res);
+      data = res.features;
       simpleEarthquakeDisplay(res.features);
+
+      console.log(data.length);
+      for(var i = 0; i < data.length; i++){
+        var lat = data[i].geometry.coordinates[1];
+        var long = data[i].geometry.coordinates[0];
+        populateMap(lat,long);
+      }
     })
     .fail(function(error) {
-      // Do something with the error
+      document.write("Error");
     })
 
-  // SAMPLE: Display the earthquake titles on the page
+  // Display the earthquake titles on the page
   function simpleEarthquakeDisplay(quakes) {
     var container = $('#sample').empty();
     quakes.forEach(function(quake) {
       var quakeEl = $('<li></li>')
         .text(quake.properties.title)
         .appendTo(container);
+
     });
   }
 });
+
+// Initialize the map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: {lat: 0, lng: 0}
+    });
+}
+
+    // Fill the map
+function populateMap(lat_val, long_val){
+    var myLatLng = {lat: lat_val, lng: long_val};
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+    });
+  }
